25dfb90736092ded8a2a46b0a510ab62e6f63b37
Bataklik <60544395+Bataklik@users.noreply.github.com>
2026-01-15 18:34:49 +0100

    feat: :sparkles: Enhance routing and error handling with templates for posts and error responses

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
-from fastapi import responses
+from fastapi import FastAPI, Request, responses, HTTPException,status
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 posts: list[dict] = [
     {
@@ -19,12 +25,53 @@ posts: list[dict] = [
     },
 ]
 
-@app.get("/",response_class=responses.HTMLResponse,include_in_schema=False)
-@app.get("/posts",response_class=responses.HTMLResponse,include_in_schema=False)
-def home():
-    return f"<h1>{posts[0]['title']}</h1>"
+#! HTTP Routes
+@app.get("/",include_in_schema=False,name="home")
+@app.get("/posts",include_in_schema=False,name="posts")
+def home(request:Request):
+    return templates.TemplateResponse(request,"home.html",
+                                      {"posts":posts,"title":"Home"})
+
+@app.get("/post/{post_id}",include_in_schema=False,name="post")
+def post(request: Request, post_id: int):
+    for post in posts:
+        if post["id"] == post_id:
+            post_title = post["title"]
+            return templates.TemplateResponse(request,"post.html",{"post":post,
+                                                                   "title":post_title})
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
 
 
+#! API Routes
 @app.get("/api/posts")
 def get_posts():
     return posts
+
+@app.get("/api/posts/{post_id}")
+def get_post(post_id: int):
+    for post in posts:
+        if post["id"] == post_id:
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+#! Error Handlers
+@app.exception_handler(StarletteHTTPException)
+def get_http_exception_handler(request: Request, exc: StarletteHTTPException):
+    message = (
+        exc.detail
+        if exc.detail
+        else "The resource you are looking for does not exist.")
+
+    if request.url.path.startswith("/api"):
+        return JSONResponse(
+            status_code=exc.status_code,
+            content={"detail": message},
+        )
+    return templates.TemplateResponse(request,
+        "error.html",
+        {
+            "status_code": exc.status_code,
+            "title": exc.status_code,
+            "message": message,
+        }
+    ,status_code=exc.status_code)
